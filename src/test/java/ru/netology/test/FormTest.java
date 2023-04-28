54a06c9b1a81f196d6951f425b36be587485f98a
Anna Kainova <annakainova88@gmail.com>
2023-04-28 20:52:54 +0200

    add WebManager

--- src/test/java/ru/netology/test/FormTest.java
+++ src/test/java/ru/netology/test/FormTest.java
@@ -8,7 +8,6 @@ import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
 import java.util.List;
@@ -30,7 +29,7 @@ public class FormTest {
         options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--no-sandbox");
         options.addArguments("--headless");
-        driver = new ChromeDriver(options);
+        driver = WebDriverManager.chromedriver().create();
     }
 
     @AfterEach
