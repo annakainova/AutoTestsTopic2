a90edf8e39f9e6490c3673288a1ad29ee1428e81
Anna Kainova <annakainova88@gmail.com>
2023-04-30 21:14:57 +0200

    delete driver

--- src/test/java/ru/netology/test/FormTest.java
+++ src/test/java/ru/netology/test/FormTest.java
@@ -20,7 +20,6 @@ public class FormTest {
 
     @BeforeAll
     static void setUpAll() {
-        System.setProperty("webdriver.chrome.driver", "./driver/win/chromedriver.exe");
         WebDriverManager.chromedriver().setup();
     }
 
